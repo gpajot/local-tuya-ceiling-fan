7a4c818406a12b49788b06af2ee2f76d05d2bcf1
Gabriel Pajot <github@gab.les-cactus.co>
2023-02-12 14:58:29 +0100

    feat: use latest local-tuya

--- local_tuya_ceiling_fan/__init__.py
+++ local_tuya_ceiling_fan/__init__.py
@@ -1,3 +1,5 @@
+import logging
+
 from local_tuya_ceiling_fan.device import (
     FanDevice,
     FanDirection,
@@ -5,3 +7,5 @@ from local_tuya_ceiling_fan.device import (
     FanSpeed,
     FanState,
 )
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
--- local_tuya_ceiling_fan/domoticz/install.py
+++ local_tuya_ceiling_fan/domoticz/install.py
@@ -1,10 +1,9 @@
-from enum import IntEnum
-from typing import Dict, Optional, Set
+from typing import Dict
 
 from local_tuya import DeviceConfig, ProtocolConfig
 from local_tuya.domoticz import (
-    Parameter,
     PluginMetadata,
+    UnitId,
     UnitManager,
     install_plugin,
     selector_switch_unit,
@@ -50,106 +49,76 @@ def _get_metadata() -> PluginMetadata:
                 },
             ]
         },
-        parameters=(
-            Parameter(field="Mode1", label="Included units", default=UnitId.names()),
-        ),
     )
 
 
-class UnitId(IntEnum):
+class FanUnitId(UnitId):
     POWER = 1
     SPEED = 2
     DIRECTION = 3
     LIGHT = 4
     MODE = 5
 
-    @classmethod
-    def names(cls) -> str:
-        return ",".join(e.name.lower() for e in cls)
-
-    @classmethod
-    def included(cls, names: Optional[str]) -> Set["UnitId"]:
-        if not names:
-            return set(cls)
-        included_names = names.split(",")
-        return {e for e in cls if e.name.lower() in included_names}
-
 
 def on_start(
     protocol_config: ProtocolConfig,
-    parameters: Dict[str, str],
+    _: Dict[str, str],
     manager: UnitManager[FanState],
 ) -> FanDevice:
-    included_units = UnitId.included(parameters.get("Mode1"))
     device = FanDevice(
         config=DeviceConfig(
             protocol=protocol_config,
             debounce_updates=1,
         ),
     )
-    if UnitId.POWER in included_units:
-        manager.register(
-            switch_unit(
-                id_=UnitId.POWER,
-                name="power",
-                image=9,
-                command_func=device.switch,
-            ),
-            lambda s: s.power,
-        )
-    else:
-        manager.remove(UnitId.POWER)
-    if UnitId.SPEED in included_units:
-        manager.register(
-            selector_switch_unit(
-                id_=UnitId.SPEED,
-                name="speed",
-                image=7,
-                enum=FanSpeed,
-                command_func=device.set_speed,
-            ),
-            lambda s: s.speed,
-        )
-    else:
-        manager.remove(UnitId.SPEED)
-    if UnitId.DIRECTION in included_units:
-        manager.register(
-            selector_switch_unit(
-                id_=UnitId.DIRECTION,
-                name="direction",
-                image=7,
-                enum=FanDirection,
-                command_func=device.set_direction,
-            ),
-            lambda s: s.direction,
-        )
-    else:
-        manager.remove(UnitId.DIRECTION)
-    if UnitId.LIGHT in included_units:
-        manager.register(
-            switch_unit(
-                id_=UnitId.LIGHT,
-                name="light",
-                image=0,
-                command_func=device.switch_light,
-            ),
-            lambda s: s.light,
-        )
-    else:
-        manager.remove(UnitId.LIGHT)
-    if UnitId.MODE in included_units:
-        manager.register(
-            selector_switch_unit(
-                id_=UnitId.MODE,
-                name="mode",
-                image=19,
-                enum=FanMode,
-                command_func=device.set_mode,
-            ),
-            lambda s: s.mode,
-        )
-    else:
-        manager.remove(UnitId.MODE)
+    manager.register(
+        switch_unit(
+            id_=FanUnitId.POWER,
+            name="power",
+            image=9,
+            command_func=device.switch,
+        ),
+        lambda s: s.power,
+    )
+    manager.register(
+        selector_switch_unit(
+            id_=FanUnitId.SPEED,
+            name="speed",
+            image=7,
+            enum=FanSpeed,
+            command_func=device.set_speed,
+        ),
+        lambda s: s.speed,
+    )
+    manager.register(
+        selector_switch_unit(
+            id_=FanUnitId.DIRECTION,
+            name="direction",
+            image=7,
+            enum=FanDirection,
+            command_func=device.set_direction,
+        ),
+        lambda s: s.direction,
+    )
+    manager.register(
+        switch_unit(
+            id_=FanUnitId.LIGHT,
+            name="light",
+            image=0,
+            command_func=device.switch_light,
+        ),
+        lambda s: s.light,
+    )
+    manager.register(
+        selector_switch_unit(
+            id_=FanUnitId.MODE,
+            name="mode",
+            image=19,
+            enum=FanMode,
+            command_func=device.set_mode,
+        ),
+        lambda s: s.mode,
+    )
     return device
 
 
@@ -158,4 +127,5 @@ if __name__ == "__main__":
         _get_metadata(),
         on_start,
         "local_tuya_ceiling_fan.domoticz.install",
+        FanUnitId,
     )
